f938a213be53f7de4ca8b209135c92ebdc61118f
shuji1911 <158697245+shuji1911@users.noreply.github.com>
2024-03-14 17:01:59 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE>
+<html lang="ja">
+
+<body>
+    <p>
+
+    <?php
+        $personal_data = ['name' =>  'onion', 'price' =>  200, 'weight' =>  160,];
+
+        print_r($personal_data);
+    ?>
+    </p>    
+</body>
+
+</html>
+
